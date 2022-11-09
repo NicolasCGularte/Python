6b4181550293712ef9cb4589b6bbf8a42eca09f1
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:31:40 -0300

    Delete ex004.py

--- ex004.py
+++
@@ -1,7 +0,0 @@
-#crie um programa que leia dois números e mostre a soma entre eles
-
-
-n1 = int(input('digite um número: '))
-n2 = int(input('digite outro número: '))
-soma = n1+n2
-print('a soma entre {} e {} é igual a {}'.format(n1, n2, soma))
