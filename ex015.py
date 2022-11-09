84187306548e55217e7218179437b6b100501493
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:35:25 -0300

    Delete ex015.py

--- ex015.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que converta uma temperatura digitando em graus celsius e converta para graus fahrenheit
-
-c = float(input('informe a temperatura em °C: '))
-f = 9*c/5+32
-print('a temperatura de {}°C corresponde a {}°F'.format(c, f))
