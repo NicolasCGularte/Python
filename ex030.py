cf5ce5c6d6be424d3ba0b8a64e585197dad0a0de
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:33:36 -0300

    Delete ex030.py

--- ex030.py
+++
@@ -1,9 +0,0 @@
-'''Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo
-que ele foi multado. A multa vai custar R$ 7,00 por cada Km acima do limite'''
-
-velocidade = float(input('Informe a velocidade do carro: '))
-if velocidade > 80:
-    print('Você foi multado')
-    multa = (velocidade-80) * 7
-    print('Sua multa é de R${:.2f}'.format(multa))
-print('Tenha um bom dia! Dirija com segurança!')
