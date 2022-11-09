2b013d0bccd97e71ff3203c84d0e9d664057c713
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:36:36 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,5 +0,0 @@
-# faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
-
-numint = int(input('digite um número'))
-print('analisando o número {}, o número sucessor é {} e o antecessor é {}'.format(numint, (numint+1), (numint-1)))
-
