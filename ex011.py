b2e1480e923bbc9bd21187b45b6d337048da9f82
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:35:48 -0300

    Delete ex011.py

--- ex011.py
+++
@@ -1,8 +0,0 @@
-# crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares
-# ela pode comprar
-
-dinheiro = float(input('informe quanto dinheiro vc tem? R$'))
-dolares = dinheiro/3.27
-print('você pode comprar {:.2f} dólares'.format(dolares))
-
-
