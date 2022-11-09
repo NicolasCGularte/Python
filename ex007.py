a9830466e07f33c7ab1c974f35da15d147e34965
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:36:23 -0300

    Delete ex007.py

--- ex007.py
+++
@@ -1,5 +0,0 @@
-# crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
-
-n = int(input('digite um número'))
-print('o dobro do número é {}, \n o triplo é {} \n e a raiz quadrada é {:.2f}'.format((n*2), (n*3), (n**(1/2))))
-
