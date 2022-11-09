c0a93edb98da8087107d411a58565d15d15b25c5
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:35:36 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,5 +0,0 @@
-# faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto
-
-preco = float(input('informe o preço do produto R$'))
-preconovo = preco - (preco/100*5)
-print('o valor de R${:.2f} agora com desconto de 5% é: R${:.2f}'.format(preco, preconovo))
