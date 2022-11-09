099bc318c58a9395e355f80122ca3c5292774df0
Nicolas C. Gularte <115746419+NicolasCGularte@users.noreply.github.com>
2022-11-09 19:34:08 -0300

    Delete ex026.py

--- ex026.py
+++
@@ -1,4 +0,0 @@
-'''Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome'''
-
-nome = str(input('Qual é o seu nome completo? ')).strip()
-print('Seu nome tem Silva? {} '.format('SILVA' in nome.upper()))
